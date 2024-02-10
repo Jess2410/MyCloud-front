d07d005c54426af356270b13bb154563d58187ff
Jessica <jess2410@hotmail.fr>
2024-02-10 15:48:52 +0100

    fix: added App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import "./App.css";
+import Button from "./components/Button/Button";
+import logo from "./assets/icons/add-file-icon.png";
+import IconButton from "./components/IconButton/IconButton";
+
 function App() {
   return (
     <>
-      <div></div>
+      <Button label={"Connectez-vous"} variant="contained" />
+      <Button label={"Inscrivez-vous"} variant="outlined" />
+      <IconButton icon={logo} />
     </>
   );
 }
