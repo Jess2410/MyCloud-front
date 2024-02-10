abfb498ee2695681e54a42cb9248af9354a5f18c
Calas <barbara.calas@orange.fr>
2024-02-10 18:10:53 +0100

    mise en place du component AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
-import Button from "./components/Button/Button";
+import Button from "./components/button/Button";
 import logo from "./assets/icons/add-file-icon.png";
 import IconButton from "./components/IconButton/IconButton";
+import AppBar from "./components/AppBar/AppBar";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Button label={"Connectez-vous"} variant="contained" />
       <Button label={"Inscrivez-vous"} variant="outlined" />
       <IconButton icon={logo} />
+      <AppBar />
     </>
   );
 }
